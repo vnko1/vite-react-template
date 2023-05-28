7358a72bacdb6799db3c6c9babcc0df0d0b5545e
Andrii Valenko <andreyvalenko@gmail.com>
2023-05-28 09:20:04 +0300

    eds

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import jsconfigPaths from "vite-jsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), jsconfigPaths()],
   base: "/vite-react-template/",
 });
